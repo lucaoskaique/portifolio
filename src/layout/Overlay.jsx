6b9b19f06052b784ffc4b10cd4b30a7ec99d903d
lucaoskaique <lucas.kaique175@hotmail.com>
2023-05-27 14:06:01 -0300

    Finished implementation

---
+++ src/layout/Overlay.jsx
@@ -0,0 +1,46 @@
+import {
+  Container,
+  TopLeft,
+  BottomLeft,
+  BottomRight,
+  Hamburger,
+} from "./styles";
+// import { VelvetBanana } from "./VelvetBanana";
+
+export default function Overlay() {
+  return (
+    <Container>
+      <TopLeft>
+        <h1>COICE —</h1>
+        <p>Exploring percussive sounds —</p>
+      </TopLeft>
+      <BottomLeft>
+        <span>
+          Instagram <a href="https://www.instagram.com/coic_e/">@coic_e</a>
+        </span>
+        <span>
+          Soundcloud <a href="https://soundcloud.com/coic_e">Soundcloud</a>
+        </span>
+      </BottomLeft>
+      <BottomRight>
+        COICE surge em 2020 como um podcast voltado para música eletrônica.
+        <br />
+        Focada em sons percussivos, valorizando a sonoridade da América Latina e
+        brasileira.
+        <br />
+        Enfatizando a importância do protagonismo local na cena da música
+        eletrônica.
+        <br />
+        A festa se propõe como uma interseção entre música, programação e artes
+        visuais/performance.
+        <br />
+      </BottomRight>
+      <Hamburger>
+        <div />
+        <div />
+        <div />
+      </Hamburger>
+      {/* <VelvetBanana /> */}
+    </Container>
+  );
+}
